160c483370a680761c4b68ed514a5c15bce660f3
sissukim-design <sissukim@gmail.com>
2026-06-18 18:56:27 -0500

    feat: add Vercel Web Analytics
    Added Analytics component to the rendering tree.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    <Analytics />
   </StrictMode>,
 );
